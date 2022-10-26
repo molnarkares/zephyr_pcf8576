c0eacb5d53f671d91461b700a57ef5ce727c198f
Karoly Molnar <molnarkares@gmail.com>
2022-10-26 17:01:25 +0200

    Update main.c
    Updated copyright notice

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
+* Copyright (c) 2022 Karoly Molnar
+* SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/drivers/gpio.h>
